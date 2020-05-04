9b233b43850ba487a4f2313b9d360a0834176831
Olivia Graham <oliviagraham@college.harvard.edu>
2020-05-03 21:18:45 -0400

    Initialize backend repo - GraphQL

---
+++ index.js
@@ -0,0 +1,60 @@
+const cors = require('cors');
+const express = require('express');
+const { ApolloServer, gql } = require('apollo-server-express');
+
+const app = express();
+
+app.use(cors());
+
+const schema = gql`
+  type Query {
+    users: [User!]
+    me: User
+    user(id: ID!): User
+  }
+  type User {
+    id: ID!
+    username: String!
+  }
+`;
+
+let users = {
+  1: {
+    id: '1',
+    username: 'Olivia Graham',
+  },
+  2: {
+    id: '2',
+    username: 'Jack Swisher',
+  },
+};
+
+const me = users[1];
+
+const resolvers = {
+  Query: {
+    users: () => {
+      return Object.values(users);
+    },
+    user: (parent, { id }) => {
+      return users[id];
+    },
+    me: (parent, args, { me }) => {
+      return me;
+    },
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs: schema,
+  resolvers,
+  context: {
+    me: users[1],
+  },
+});
+
+server.applyMiddleware({ app, path: '/graphql' });
+
+app.listen({ port: 8000 }, () => {
+  console.log('Apollo Server on http://localhost:8000/graphql');
+});
